5e318486d16676440802af303e17b068ae952413
Ilya <cankillah@gmail.com>
2026-01-06 18:16:22 +0200

    add tmp request

--- app.js
+++ app.js
@@ -7,20 +7,30 @@ app.use(express.json());
 const API = "http://mediamtx:8889/v3";
 
 app.post("/camera/start", async (_, res) => {
-  const r = await fetch(`${API}/paths/add/camera`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      source: "rtsp://admin:dima_password_0@192.168.1.105:554/cam/realmonitor?channel=1&subtype=1",
-      sourceOnDemand: true
-    })
-  });
-  res.json(await r.json());
+    const r = await fetch(`${API}/paths/add/camera`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            source: "rtsp://admin:dima_password_0@192.168.1.105:554/cam/realmonitor?channel=1&subtype=1",
+            sourceOnDemand: true
+        })
+    });
+    res.json(await r.json());
 });
 
 app.get("/camera/status", async (_, res) => {
-  const r = await fetch(`${API}/paths/get/camera`);
-  res.json(await r.json());
+    const r = await fetch(`${API}/paths/get/camera`);
+    res.json(await r.json());
 });
 
+app.get("tmpcreate", async (_, res) => {
+    await fetch("http://mediamtx:8889/v3/paths/add/camera", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            source: "rtsp://admin:dima_password_0@192.168.1.105:554/cam/realmonitor?channel=1&subtype=1",
+            sourceOnDemand: false
+        })
+    });
+});
 app.listen(process.env.PORT || 3000);
